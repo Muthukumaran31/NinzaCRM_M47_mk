dde90d237ec12b97b2a3276d7a7eef8c2c533d97
ACER <ACER@DESKTOP-GVSLSAS>
2026-03-12 07:51:58 +0530

    Fist time commit_mk

---
+++ src/test/java/Practice/Demo1Test.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	@Test(groups = "smoke")
+	public void bikeTest()
+	{
+		System.out.println("RE");
+	}
+
+}
---
+++ src/test/java/Practice/DemoTest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void catTest()
+	{
+		System.out.println("BMW");
+	}
+
+}
